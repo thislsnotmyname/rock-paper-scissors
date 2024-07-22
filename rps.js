6d880b41cb89c51e89c54c9387b9d68319e0ce26
JM <143664295+thislsnotmyname@users.noreply.github.com>
2024-07-22 11:22:07 -0500

    Create human choice logic

--- rps.js
+++ rps.js
@@ -1,6 +1,18 @@
 "use strict";
 // JM, 07/22/2024
+const CHOICES = ["rock", "paper", "scissors"];
 let getComputerChoice = () => {
-    const CHOICES = ["Rock", "Paper", "Scissors"];
     return CHOICES[Math.floor(Math.random() * CHOICES.length)]
 }
+let getHumanChoice = () => {
+    let humanChoice = prompt("Rock, Paper, or Scissors?").toLowerCase();
+    
+    if (CHOICES.includes(humanChoice)) {
+        return humanChoice;
+    } else {
+        alert("That's not a choice!");
+        return getHumanChoice();
+    }
+}
+console.log(getComputerChoice());
+console.log(getHumanChoice());
