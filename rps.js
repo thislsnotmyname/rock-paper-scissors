c8394f1e0c8d5137ef668d69907a68bfe74ce84c
JM <143664295+thislsnotmyname@users.noreply.github.com>
2024-07-23 10:41:03 -0500

    Create logic to play a round
    Also reworked the way the human choice function handles
    case-insensitivity

--- rps.js
+++ rps.js
@@ -1,11 +1,20 @@
 "use strict";
 // JM, 07/22/2024
-const CHOICES = ["rock", "paper", "scissors"];
+const CHOICES = ["Rock", "Paper", "Scissors"];
+let humanScore = 0;
+let computerScore = 0;
+let tieScore = 0;
+
+let capitalizeFirstLetter = (str) => {
+    return `${str.slice(0, 1).toUpperCase()}${str.slice(1, str.length).toLowerCase()}`;
+}
+
 let getComputerChoice = () => {
     return CHOICES[Math.floor(Math.random() * CHOICES.length)]
 }
+
 let getHumanChoice = () => {
-    let humanChoice = prompt("Rock, Paper, or Scissors?").toLowerCase();
+    let humanChoice = capitalizeFirstLetter(prompt("Rock, Paper, or Scissors?"));
     
     if (CHOICES.includes(humanChoice)) {
         return humanChoice;
@@ -14,5 +23,28 @@ let getHumanChoice = () => {
         return getHumanChoice();
     }
 }
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+
+let playRound = (humanChoice, computerChoice) => {
+    
+    if (humanChoice === computerChoice) {
+        console.log(`Tie! ${humanChoice} ties ${computerChoice}.`);
+        tieScore += 1;
+        return `Tie! ${humanChoice} ties ${computerChoice}.`;
+    } else if (
+
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+
+    ) {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+        humanScore += 1;
+        return `You win! ${humanChoice} beats ${computerChoice}.`;
+    } else {
+        console.log(`Computer wins! ${computerChoice} beats ${humanChoice}.`);
+        computerScore += 1;
+        return `Computer wins! ${computerChoice} beats ${humanChoice}.`;
+    }
+}
+
+playRound(getHumanChoice(), getComputerChoice());
