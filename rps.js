6d267839ce1f5f44995861cf0dd3f67dff6c4dd9
JM <143664295+thislsnotmyname@users.noreply.github.com>
2024-07-24 20:32:29 -0500

    Create basic DOM integration

--- rps.js
+++ rps.js
@@ -1,6 +1,13 @@
 "use strict";
-// JM, 07/23/2024
+// JM, 07/24/2024
 const CHOICES = ["Rock", "Paper", "Scissors"];
+
+let humanScore = 0;
+let computerScore = 0;
+
+const BUTTONS = document.querySelector("#playing-field");
+const GAME_TEXT = document.querySelector("#game-text");
+const RESULTS = document.querySelector("#results");
 
 let capitalizeFirstLetter = (str) => {
     return `${str.slice(0, 1).toUpperCase()}${str.slice(1, str.length).toLowerCase()}`;
@@ -10,54 +17,67 @@ let getComputerChoice = () => {
     return CHOICES[Math.floor(Math.random() * CHOICES.length)]
 }
 
-let getHumanChoice = () => {
-    let humanChoice = capitalizeFirstLetter(prompt("Rock, Paper, or Scissors?"));
-    
-    if (CHOICES.includes(humanChoice)) {
-        return humanChoice;
-    } else {
-        alert("That's not a choice!");
-        return getHumanChoice();
-    }
+let getHumanChoice = (choice) => {
+    return capitalizeFirstLetter(choice);
 }
 
-let playGame = (outOf) => {
-    const WINROUNDS = Math.floor(outOf / 2) + 1;
-    let humanScore = 0;
-    let computerScore = 0;
+let updateResults = () => {
+    RESULTS.textContent = `Human: ${humanScore} | Computer: ${computerScore}`;
+}
 
-    let playRound = (humanChoice, computerChoice) => {
-    
-        if (humanChoice === computerChoice) {
+let changeGameText = (humanChoice, computerChoice, winner) => {
+    switch (winner) {
+        case "Tie":
+            GAME_TEXT.textContent = `Tie! ${humanChoice} ties ${computerChoice}.`;
             console.log(`Tie! ${humanChoice} ties ${computerChoice}.`);
-            return playRound(getHumanChoice(), getComputerChoice());
-        } else if (
-
-            (humanChoice === "Rock" && computerChoice === "Scissors") ||
-            (humanChoice === "Paper" && computerChoice === "Rock") ||
-            (humanChoice === "Scissors" && computerChoice === "Paper")
-    
-        ) {
+            break;
+        case "Human":
+            GAME_TEXT.textContent = `You win! ${humanChoice} beats ${computerChoice}.`;
             console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
-            humanScore += 1;
-            return `You win! ${humanChoice} beats ${computerChoice}.`;
-        } else {
+            break;
+        case "Computer":
+            GAME_TEXT.textContent = `Computer wins! ${computerChoice} beats ${humanChoice}.`;
             console.log(`Computer wins! ${computerChoice} beats ${humanChoice}.`);
-            computerScore += 1;
-            return `Computer wins! ${computerChoice} beats ${humanChoice}.`;
-        }
+            break;
+    }
+};
+
+let playRound = (humanChoice, computerChoice) => {
+    
+    if (humanChoice === computerChoice) {
+        changeGameText(humanChoice, computerChoice, "Tie");
+    } else if (
+        (humanChoice === "Rock" && computerChoice === "Scissors") ||
+        (humanChoice === "Paper" && computerChoice === "Rock") ||
+        (humanChoice === "Scissors" && computerChoice === "Paper")
+    ) {
+        changeGameText(humanChoice, computerChoice, "Human");
+        humanScore += 1;
+    } else {
+        changeGameText(humanChoice, computerChoice, "Computer");
+        computerScore += 1;
     }
 
-    console.log(`Let's play Rock, Paper, Scissors, best ${WINROUNDS} out of ${outOf}.`);
-    do {
-        playRound(getHumanChoice(), getComputerChoice());
-    } while (humanScore < WINROUNDS && computerScore < WINROUNDS)
-    
-    if (humanScore > computerScore) {
-        return `You win! (${humanScore} to ${computerScore})`;
-    } else {
-        return `Computer wins! (${computerScore} to ${humanScore})`;
-    }
+    updateResults();
 }
 
-console.log(playGame(5));
+let initGame = (outOf) => {
+    const WINROUNDS = Math.floor(outOf / 2) + 1;
+    GAME_TEXT.textContent = `Let's play Rock, Paper, Scissors, best ${WINROUNDS} out of ${outOf}.`;
+    updateResults();
+    // if (humanScore === WINROUNDS || computerScore === WINROUNDS) {
+    //     if (humanScore > computerScore) {
+    //         return `You win! (${humanScore} to ${computerScore})`;
+    //     } else {
+    //         return `Computer wins! (${computerScore} to ${humanScore})`;
+    //     }
+    // }
+
+}
+
+BUTTONS.addEventListener("click", (e) => {
+    if (e.target.nodeName !== "BUTTON") return;
+    playRound(getHumanChoice(e.target.id), getComputerChoice());
+});
+
+initGame(5);
